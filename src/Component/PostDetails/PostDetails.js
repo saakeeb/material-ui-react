b5ef5d665636d66b7df5aa4ca2de7a0fed7e088d
saakeeb <saakeeb752@gmail.com>
2020-12-01 00:25:59 +0600

    project run

--- src/Component/PostDetails/PostDetails.js
+++ src/Component/PostDetails/PostDetails.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import FakeData from '../Component/FakeData/FakeData';
+import FakeData from '../FakeData/FakeData';
+import Post from '../Post/Post';
 
 const PostDetails = () => {
     const {postIdNum} = useParams();
     const postId = FakeData.find(pd => pd.id === parseInt(postIdNum));
     console.log(postId);
     return (
-        <div>
+        <div style={{marginTop: '3.9rem'}}>
             <h3>{postIdNum} no details here</h3>
+            <Post posts={postId}></Post>
 
         </div>
     );
